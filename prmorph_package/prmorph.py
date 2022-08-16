2124db8c75e77f2c29f609e9abdb9b724d332a28
Noah Cape <noahcape@Noahs-MacBook-Pro.local>
2022-08-16 12:12:03 -0400

    start of optmzing vision

--- prmorph_package/prmorph.py
+++ prmorph_package/prmorph.py
@@ -1,4 +1,5 @@
 """Main module."""
+import typing as type
 import datetime as dt
 import os
 from . import args
@@ -7,6 +8,9 @@ from . import cli as cli
 from .functions import main as funcm
 
 logger = logs.get_logger(__name__)
+
+# do not use vision rn
+VISION = False
 
 """ This is the main entry point from cli to functions """
 def main(in_dir: str, out_dir: str):
@@ -17,28 +21,33 @@ def main(in_dir: str, out_dir: str):
         logger.info("prmorph running")
         logger.info(f"reading from {in_dir}, writing to {out_dir}")
 
-        """ get the date for the file """
-        date = dt.date.today().strftime('%y%m%d')
-        file_name = f"{out_dir}/computed_lengths_{date}.csv"
+        logger.info("Computing Regular Length")
+        file_name = execute_workflow(in_dir, out_dir, "computed_lengths", funcm.regular_length)
+        logger.info(f"DONE: regular length computation, find file at {file_name}")
 
-        """ open file to write to """
-        with open(file_name, 'w') as writer:
-            # """ add header """
-            # writer.write("file_name,length (mm)\n")
+        if VISION:
+            logger.info("Renaming Images")
+            file_name = execute_workflow(in_dir, out_dir, "renamed_files", funcm.detect_fish_id)
+            logger.info(f"DONE: detecting fish IDs, find file at {file_name}")
 
-            """ loop through in_dir """
-            for img in os.listdir(in_dir):
-
-                """ ensure that is is a valid image file to read """
-                if img.lower().endswith(('.png', '.jpg', '.jpeg')):
-                    funcm.detect_fish_id(f"{in_dir}/{img}", out_dir)
-
-                    """ compute the length """
-                    funcm.regular_length(f"{in_dir}/{img}", writer)
-
-        logger.info(f"prmorph finished computing regular length, find csv file at {file_name}")
-    
     except Exception as e:
         logger.error(str(e))
         raise
-    
+
+def execute_workflow(in_dir: str, out_dir: str, file_desc: str, method: type.Callable):
+    """ get the date for the file """
+    date = dt.date.today().strftime('%y%m%d')
+    file_name = f"{out_dir}/{file_desc}_{date}.csv"
+
+    """ open file to write to """
+    with open(file_name, 'w') as writer:
+
+        """ loop through in_dir """
+        for img in os.listdir(in_dir)[:5]:
+
+            """ ensure that is is a valid image file to read """
+            if img.lower().endswith(('.png', '.jpg', '.jpeg')):
+                """ compute the length """
+                method(f"{in_dir}/{img}", writer, out_dir)
+
+    return file_name
