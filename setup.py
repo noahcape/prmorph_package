c74b91c63b9cea5d99b086bc372fe4edf121c1a1
Noah Cape <69659089+noahcape@users.noreply.github.com>
2022-08-19 12:23:18 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/noahcape/prmorph_package',
-    version='0.0.3',
+    version='0.0.4',
     zip_safe=False,
 )
